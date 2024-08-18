edc3ae4e5bdbb89f97cdc83195170a6e7f090b70
meetpatel1812 <71310616+meetpatel1812@users.noreply.github.com>
2024-08-18 02:44:29 -0400

    Add files via upload

---
+++ dataframe.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+# Display Title and Description
+st.title("Vendor Management Portal")
+st.markdown("Enter the details of the new vendor below.")
+# Establishing a Google Sheets connection
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+# Fetch existing vendors data
+existing_data = conn.read(worksheet="Sheet1", usecols=list(range(2)), ttl=5)
+existing_data = existing_data.dropna(how="all")
+st.dataframe(existing_data)
