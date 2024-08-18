e09ba84f41f1191bc0dcdab0a59de6f9a354f7fe
meetpatel1812 <71310616+meetpatel1812@users.noreply.github.com>
2024-08-18 03:51:16 -0400

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_excel("test.xlsx", engine='openpyxl')
+df = pd.read_excel("https://1drv.ms/x/s!ApEp4lqnmodmhnda293x2jLaGTtP?e=gYKaje&nav=MTVfezAwMDAwMDAwLTAwMDEtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMH0", engine='openpyxl')
 st.dataframe(df)
